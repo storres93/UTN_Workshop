b1df3ca9dc2e65941a5a3fad5904e4c4b17dff33
Sebastian Torres <storres@medallia.com>
2017-06-26 22:37:20 -0300

    feat(*): add npm dependencies

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
 var rootElement = React.createElement('h1', {}, "Hola Mundo!");
 
 ReactDOM.render(rootElement, document.getElementById('react-app'));
